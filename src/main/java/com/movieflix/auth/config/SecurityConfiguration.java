f8653b40aaa470519fda2dc0387b042fc823379b
Avijeet Shil <avijeetshil110@gmail.com>
2024-02-11 22:51:13 -0600

    Till before Auth Controller

---
+++ src/main/java/com/movieflix/auth/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.movieflix.auth.config;
+
+import com.movieflix.auth.services.AuthFilterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author Avijeet
+ * @project movieApi
+ * @github avijeetas
+ * @date 02-11-2024
+ **/
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfiguration {
+   private final AuthenticationProvider authenticationProvider;
+   private final AuthFilterService authFilterService;
+
+   @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+      httpSecurity
+              .csrf(AbstractHttpConfigurer::disable)
+              .authorizeHttpRequests(auth->
+                      auth.requestMatchers("/api/v1/auth/**")
+                              .permitAll()
+                              .anyRequest()
+                              .authenticated())
+              .sessionManagement(session -> session
+                      .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+              .authenticationProvider(authenticationProvider)
+              .addFilterBefore(authFilterService, UsernamePasswordAuthenticationFilter.class);
+      return  httpSecurity.build();
+   }
+}
--- src/main/java/com/movieflix/utils/AppConstants.java
+++ src/main/java/com/movieflix/utils/AppConstants.java
@@ -12,7 +12,7 @@ public class AppConstants {
 
    public static final String MOVIE_NOT_FOUND_WITH_ID_MSG = "Movie not found with id = %s";
    public static final String FILE_UPLOADED_MSG = "File uploaded with filename %s";
-   public static final String POSTER_URL_NOT_BLANK_OR_NULL_MSG = "Posterurl must not be blank or null";
+   public static final String POSTER_URL_NOT_BLANK_OR_NULL_MSG = "Posterurl should not be blank or null";
 
    public static final String POSTER_SHOULD_NOT_BE_NULL_OR_BLANK = "Poster should not be null or blank";
    public static final String DESCRIPTION_SHOULD_NOT_BE_NULL_OR_BLANK = "Description should not be null or blank";
@@ -21,4 +21,6 @@ public class AppConstants {
    public static final String COUNTRIES_SHOULD_NOT_BE_NULL_OR_BLANK = "Countries should not be null or blank";
    public static final String NAME_SHOULD_NOT_BE_NULL_OR_BLANK = "Name should not be null or blank";
    public static final String TITLE_SHOULD_NOT_BE_NULL_OR_BLANK = "Title should not be null or blank";
+   public static final String USER_NAME_SHOULD_NOT_BE_NULL_OR_BLANK = "Username should not be null or blank";
+   public static final String USER_NOT_FOUND_WITH_EMAIL = "User not found with email %s";
 }
